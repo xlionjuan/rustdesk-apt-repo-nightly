8370ee204bef291c856ba0cc39e9589adfb33325
XLion <xlion@xlion.tw>
2024-09-21 21:49:00 +0800

    setup

--- rustdesk_latest.sh
+++ rustdesk_latest.sh
@@ -17,6 +17,9 @@ fi
 # Use jq to parse JSON data and find the asset URL
 RUSTDESK_URL_AMD64=$(echo "$RELEASE_DATA" | jq -r '.assets[] | select(.name | contains("x86_64") and endswith(".deb") and (contains("sciter") | not)) | .browser_download_url' | head -n 1)
 RUSTDESK_URL_ARM64=$(echo "$RELEASE_DATA" | jq -r '.assets[] | select(.name | contains("aarch64") and endswith(".deb") and (contains("sciter") | not)) | .browser_download_url' | head -n 1)
+RUSTDESK_URL_ARMHF=$(echo "$RELEASE_DATA" | jq -r '.assets[] | select(.name | contains("aarch64") and endswith(".deb")) | .browser_download_url' | head -n 1)
+# armhf is ARMv7, and only has sciter version.
+
 # Check if the asset URL was found
 #if [ -z "$RUSTDESK_URL" ]; then
 #    echo "No matching file found."
@@ -27,7 +30,9 @@ RUSTDESK_URL_ARM64=$(echo "$RELEASE_DATA" | jq -r '.assets[] | select(.name | co
 echo "--------------------RESULT--------------------"
 echo "RUSTDESK_URL_AMD64=\"$RUSTDESK_URL_AMD64\""
 echo "RUSTDESK_URL_ARM64=\"$RUSTDESK_URL_ARM64\""
+echo "RUSTDESK_URL_ARMHF=\"$RUSTDESK_URL_ARMHF\""
 echo ""
 echo "------------------DOWNLOADING-----------------"
 wget $RUSTDESK_URL_AMD64
 wget $RUSTDESK_URL_ARM64
+wget $RUSTDESK_URL_ARMHF
