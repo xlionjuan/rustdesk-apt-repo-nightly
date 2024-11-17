7d88df4f26d294ee6aafe6c4844a29bf546e6fb6
XLion <xlion@xlion.tw>
2024-11-17 17:51:53 +0800

    opti deb-reversion.sh

--- deb-reversion.sh
+++ deb-reversion.sh
@@ -2,13 +2,9 @@
 
 set -oue pipefail
 
-current_dir=$(pwd) # Get the current working directory
-SOURCE_DIR="$current_dir/ori" # Directory containing the original .deb files
-TARGET_DIR="$current_dir" # Target directory for modified .deb files
+CURRENT_DIR="$(pwd)"
+SOURCE_DIR="$(pwd)/ori" # Directory containing the original .deb files
 DATE=$(date +%Y%m%d) # Current date in YYYYMMDD format
-
-# Ensure the target directory exists
-mkdir -p "$TARGET_DIR"
 
 # Iterate over all .deb files in the source directory
 for deb_file in "$SOURCE_DIR"/*.deb; do
@@ -24,8 +20,6 @@ for deb_file in "$SOURCE_DIR"/*.deb; do
 
             # Remove the original file after processing
             rm -f "$deb_file"
-
-            echo "Processed and moved: $TARGET_DIR/$(basename "$deb_file")"
         } &
     else
         echo "Skipping: $deb_file"
@@ -35,6 +29,9 @@ done
 # Wait for all parallel tasks to complete
 wait
 
-echo "Done, all modified files moved to: $TARGET_DIR"
+echo "deb-reversion works are done, all modified files are in: $CURRENT_DIR"
 echo "Cleanup......."
 rm -r "$SOURCE_DIR"
+echo "Run ls"
+ls
+
